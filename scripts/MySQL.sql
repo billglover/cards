769b5f48e2140520e62ee23329e551e8f99860c8
Bill Glover <bill@billglover.co.uk>
2017-10-18 21:37:13 +0100

    Add create script for decks table

--- scripts/MySQL.sql
+++ scripts/MySQL.sql
@@ -1,4 +1,14 @@
+DROP TABLE IF EXISTS `cards`;
+
 CREATE TABLE `cards` (
+   `uid` SERIAL,
+   `title` VARCHAR(64) NULL DEFAULT NULL,
+   PRIMARY KEY (`uid`)
+);
+
+DROP TABLE IF EXISTS `decks`;
+
+CREATE TABLE `decks` (
    `uid` SERIAL,
    `title` VARCHAR(64) NULL DEFAULT NULL,
    PRIMARY KEY (`uid`)
